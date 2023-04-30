c3f8998f2f373f6641d50cb0a0d6ef754dfa983b
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-04-30 15:46:09 +0300

    creating project dapp

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
+
+task("account", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ether.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+
 module.exports = {
   solidity: "0.8.18",
 };
